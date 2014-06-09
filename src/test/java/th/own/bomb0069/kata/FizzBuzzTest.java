d1a616fbbc679c4a8f377fd4923ad7e8e0d9ed2f
Karan Sivarat <bomb0069@gmail.com>
2014-06-09 10:32:03 -0400

    refactor test reduce duplicate code

--- src/test/java/th/own/bomb0069/kata/FizzBuzzTest.java
+++ src/test/java/th/own/bomb0069/kata/FizzBuzzTest.java
@@ -4,16 +4,16 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class FizzBuzzTest {
+
+	FizzBuzz fizzBuzz = FizzBuzzFactory.create();
 	
 	@Test
 	public void test1ShouldbeReturn1 () {
-		FizzBuzz fizzBuzz = FizzBuzzFactory.create();
 		assertEquals("1", fizzBuzz.say(1));
 	}
 
 	@Test
 	public void test2ShouldbeReturn2 () {
-		FizzBuzz fizzBuzz = FizzBuzzFactory.create();
 		assertEquals("2", fizzBuzz.say(2));
 	}
 }
